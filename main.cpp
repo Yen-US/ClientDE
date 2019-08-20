e5c9c51c0d26a67081298d247f910f9e20648e93
yenus <yen21000@gmail.com>
2019-08-19 22:36:16 -0600

    Se añade el código del Cliente

--- main.cpp
+++ main.cpp
@@ -1,19 +1,24 @@
+/**
+ *Basado en: https://www.geeksforgeeks.org/socket-programming-cc/
+ */
+
 #include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+//#include <Qt>
 #define PORT 8080
 
 int main(int argc, char const *argv[])
 {
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
+    char *hello = "Hola desde el cliente";
     char buffer[1024] = {0};
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        printf("\n Socket creation error \n");
+        printf("\n Error en la creación del socket \n");
         return -1;
     }
 
@@ -23,18 +28,19 @@ int main(int argc, char const *argv[])
     // Convert IPv4 and IPv6 addresses from text to binary form
     if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
     {
-        printf("\nInvalid address/ Address not supported \n");
+        printf("\n Dirección Invalida/No soportada \n");
         return -1;
     }
 
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
-        printf("\nConnection Failed \n");
+        printf("\nConección Fallida \n");
         return -1;
     }
     send(sock , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
+    printf("Mensaje Hola enviado\n");
     valread = read( sock , buffer, 1024);
     printf("%s\n",buffer );
+
     return 0;
 }
